790f9937d451946070b31bdbfbd1bf098927ec18
James Pai <jamespai77@gmail.com>
2023-04-01 20:40:15 -0400

    added comments

--- src/com/bleumuse/Lunar/Expr.java
+++ src/com/bleumuse/Lunar/Expr.java
@@ -2,20 +2,44 @@ package com.bleumuse.lunar;
 
 import java.util.List;
 
+/**
+ * Defines the context-free grammars of the Lunar language. Each expression is
+ * composed of a set of productions which include terminals and non-terminals. A terminal 
+ * represents an individual lexeme or token and no further. A non-terminal recurses back
+ * and leads to another set of rules. The resulting derivation or sequence of string from 
+ * the set of productions is terminated with a semicolon.
+ */
 abstract class Expr {
+
+  /**
+   * The Visitor interface defines the set of abstract methods
+   * to perform operations for each of the grammar structures. Additional
+   * implementing Visitor subclasses must have its behavior declared here.
+   */
   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
   }
+
+  /**
+   * Binary expr is a recursive structure and may be used to generate 
+   * a new Expression of any of the defined types. The Token operator 
+   * selects for a valid token and splits the two operand Expressions
+   * on the LHS and RHS.
+   */
   static class Binary extends Expr {
+    // Binary expr constructor 
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
     }
 
+    /**
+     * Performs task on Binary Expression
+     */
     @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitBinaryExpr(this);
@@ -25,11 +49,20 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
+
+  /**
+   * The Grouping grammar production is a recursive production and wraps 
+   * one or more parenthesis around a single Expression.
+   */
   static class Grouping extends Expr {
+    // Grouping expr constructor 
     Grouping(Expr expression) {
       this.expression = expression;
     }
 
+    /**
+     * Performs task on Grouping Expression
+     */
     @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitGroupingExpr(this);
@@ -37,11 +70,21 @@ abstract class Expr {
 
     final Expr expression;
   }
+
+  /**
+   * The Literal is a terminal production and maps to a single
+   * symbol. Literals includes NUMBER, STRING, (whose text representation
+   * is subject to vary), boolean TRUE, FALSE, and NIL.
+   */
   static class Literal extends Expr {
+    // Literal constructor 
     Literal(Object value) {
       this.value = value;
     }
 
+    /**
+     * Performs task on Literal 
+     */
     @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitLiteralExpr(this);
@@ -49,12 +92,21 @@ abstract class Expr {
 
     final Object value;
   }
+
+  /**
+   * The Unary grammar structure maps to a single Token operator
+   * on LHS and is followed by an Expression on the RHS.
+   */
   static class Unary extends Expr {
+    // Unary expr constructor
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
     }
 
+    /**
+     * Performs task on Unary Expression
+     */
     @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitUnaryExpr(this);
@@ -64,5 +116,6 @@ abstract class Expr {
     final Expr right;
   }
 
+  // each visitor class must implement the abstract accept() method
   abstract <R> R accept(Visitor<R> visitor);
 }
